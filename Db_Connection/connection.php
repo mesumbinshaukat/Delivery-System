cd05049fc8830e6702e49d4dcf7fb82586da736e
Huzaifa1509 <119890303+Huzaifa1509@users.noreply.github.com>
2023-12-14 21:04:22 +0500

    partials file seperated

--- Db_Connection/connection.php
+++ Db_Connection/connection.php
@@ -1,3 +1,7 @@
 <?php
 
-$conn = mysqli_connect("localhost:8080", "root", "", "delivio_db") or die("Database Connection Failed." + mysqli_error($conn));
+// $conn = mysqli_connect("localhost:8080", "root", "", "delivio_db") or die("Database Connection Failed." + mysqli_error($conn));
+
+$conn = mysqli_connect("localhost", "root", "", "delivio_dbbb") or die("Database Connection Failed." + mysqli_error($conn));
+
+?>
